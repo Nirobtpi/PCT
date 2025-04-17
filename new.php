5a5319f1d487e7af24f93b9d1b9a8903abb464da
Nirobtpi <saifasabanirob@gmail.com>
2025-04-18 01:01:51 +0600

    trait

--- new.php
+++ new.php
@@ -21,6 +21,31 @@ class Son extends Father{
 $s = new Son();
 // $s->print100();
 $s->demo();
+echo "\n";
+
+interface SayHello{
+    public function SayName();
+
+}
+class Nirob implements SayHello{
+    public function SayName(){
+        echo "Nirob";
+    }
+}
+$n=new Nirob();
+$n->SayName();
+echo "\n";
+
+trait Hello{
+    public function SayHello(){
+        echo "Hello Chikcu";
+    }
+}
+class Nirob1{
+    use Hello;
+}
+$nirob1=new Nirob1();
+$nirob1->SayHello();
 
 
 ?>
